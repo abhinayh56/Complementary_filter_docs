6d7ee9ba46741167b2fbb851f5212dcbf326e446
abhinayh56 <kumarabhinay53@gmail.com>
2026-08-11 11:44:56 +0530

    1

--- navtreedata.js
+++ navtreedata.js
@@ -24,7 +24,7 @@
 */
 var NAVTREE =
 [
-  [ "My Project", "index.html", [
+  [ "Complementary Filter", "index.html", [
     [ "Namespaces", "namespaces.html", [
       [ "Namespace List", "namespaces.html", "namespaces_dup" ]
     ] ],
